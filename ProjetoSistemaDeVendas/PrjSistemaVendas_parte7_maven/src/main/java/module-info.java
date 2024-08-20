de3f364264a833802879b5fed1d5a325449f7098
juhachmann <juliana.hachmann@gmail.com>
2024-08-19 23:20:31 -0300

    MAJOR CHANGE: Projeto adaptado para uso com Maven e MySQL
    Resolvido erro de incompatible type for field columnCount na tela de relatório alterando o jrxml, mudando de COLUMN_7 para categoria e usando um alias categoria na query declarada no mesmo arquivo. Necessário recompilar o .jasper correspondente.

--- ProjetoSistemaDeVendas/PrjSistemaVendas_parte7_maven/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module br.edu.ifsc.fln.mainapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires jasperreports;
-    
-    opens br.edu.ifsc.fln.mainapp to javafx.fxml;
-    exports br.edu.ifsc.fln.mainapp;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# POO-TDS-2022-02
-Projetos desenvolvidos na disciplina de POO do curso Técnico em Desenvolvimento de Sistemas - Ano 2022 Semestre 02
